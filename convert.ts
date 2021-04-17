f7c9b2a804467b33022d4fdea273aaf41f1c9334
Michael Buen <ienablemuch@gmail.com>
2021-04-17 22:19:21 +0800

    generated hanzi json lookup files

--- convert.ts
+++ convert.ts
@@ -73,12 +73,79 @@ async function generateHanziLookup() {
     await generateMainCopy();
     cleanUp();
     // console.log(hzl);
-    createHanziAllJsonFile();
+
+    await createHanziAllJsonFile();
+
+    await createHanziPinyinLookupFile();
+    await createHanziConversionLookupFiles();
+    await createHanziHskLookupFile();
 
     return;
 
     async function createHanziAllJsonFile() {
-        await writeJson("./hanzi-all.json", hzl, { spaces: 2 });
+        await writeJson("./lookup-all.json", hzl, { spaces: 2 });
+    }
+
+    async function createHanziPinyinLookupFile() {
+        interface IHanziPinyinLookup {
+            [hanzi: string]: string;
+        }
+
+        const hpl: IHanziPinyinLookup = {};
+
+        // prettier-ignore
+        for (const [hanzi, {pinyin}] of Object.entries(hzl)) {
+            const cleanedPinyin = pinyin?.[0]?.split(' ')?.join('');
+            if (!cleanedPinyin) {
+                continue;
+            }
+            hpl[hanzi] = cleanedPinyin;
+        }
+
+        // console.log(hpl);
+
+        await writeJson("./lookup-hanzi-pinyin.json", hpl);
+    }
+
+    async function createHanziConversionLookupFiles() {
+        // use for both simplified and traditional
+        interface IHanziConversionLookup {
+            [hanzi: string]: string;
+        }
+
+        const hclSimToTra: IHanziConversionLookup = {};
+        const hclTraToSim: IHanziConversionLookup = {};
+        for (const [hanzi, { type, aka }] of Object.entries(hzl)) {
+            if (type === "S") {
+                hclSimToTra[hanzi] = aka as string;
+            } else if (type === "T") {
+                hclTraToSim[hanzi] = aka as string;
+            }
+        }
+
+        // console.log(hclSimToTra);
+        // console.log(hclTraToSim);
+
+        await writeJson("./lookup-sim-tra.json", hclSimToTra);
+        await writeJson("./lookup-tra-sim.json", hclTraToSim);
+    }
+
+    async function createHanziHskLookupFile() {
+        interface IHanziHskLookup {
+            [hanzi: string]: number;
+        }
+
+        const hhl: IHanziHskLookup = {};
+
+        for (const [hanzi, { hsk }] of Object.entries(hzl)) {
+            if ((hsk ?? 0) > 0) {
+                hhl[hanzi] = hsk as number;
+            }
+        }
+
+        // console.log(hhl);
+
+        await writeJson("./lookup-hanzi-hsk.json", hhl);
     }
 
     function cleanUp() {
