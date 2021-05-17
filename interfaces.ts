68fca877976d1953b3537d4823e2a00505c199ee
Michael Buen <ienablemuch@gmail.com>
2021-05-17 11:01:21 +0800

    documenting

--- interfaces.ts
+++ interfaces.ts
@@ -73,3 +73,109 @@ export interface ICorrection {
 export interface IHanziPinyinEnglishLookup {
     [hanziPinyin: string]: English[];
 }
+
+// pinyin should already be processed, that is this..
+// Mài kè ěr_Qiáo dān
+// ..should be this:
+// Màikè'ěr Qiáodān
+
+// The english should concatenated. Example:
+/*
+ "行政部门": {
+    "type": "S",
+    "aka": "行政部門",
+    "pinyinEnglish": {
+      "xíng zhèng bù mén": [
+        "administrative department",
+        "administration",
+        "executive (government branch)"
+      ]
+    },
+    "english": [
+      "administrative department",
+      "administration",
+      "executive (government branch)"
+    ],
+    "source": "BBs$",
+    "pinyin": [
+      "xíng zhèng_bù mén"
+    ]
+  },
+
+  "瞭": {
+    "hsk": 4,
+    "type": "B",
+    "aka": "了",
+    "pinyinEnglish": {
+      "liǎo": [
+        "(of eyes) bright",
+        "clear-sighted",
+        "to understand clearly"
+      ],
+      "liào": [
+        "to watch from a height or distance",
+        "to survey"
+      ],  
+
+  }
+
+  "了": {
+    "hsk": 1,
+    "type": "B",
+    "aka": "瞭",
+    "pinyinEnglish": {
+      "le": [
+        "(modal particle intensifying preceding clause)",
+        "(completed action marker)"
+      ],
+      "liǎo": [
+        "to finish",
+        "to achieve",
+        "variant of 瞭|了[liao3]",
+        "to understand clearly",
+        "(of eyes) bright",
+        "clear-sighted"
+      ]
+    },  
+
+
+Type B are not shareable, they have their own line in the dictionary
+Type S and T are shareable
+
+
+Index:
+{
+    行政部门: 168
+    行政部門: 168
+    瞭: 520
+    了: 1314
+}
+
+
+
+[
+    // line 168
+    [
+        {p: 'xíngzhèng bùmén': e: 'administrative department. executive (government branch)'}
+    ],
+
+    // 520
+    [
+        {p: "liǎo", "(of eyes) bright. clear-sighted. to understand clearly },
+        {p: "liào": "to watch from a height or distance. to survey"}  
+    ]
+
+    // line 1314
+    [
+        {p: "le", e: "(modal particle intensifying preceding clause). (completed action marker)"},
+        {p: "liǎo", e: "to finish. to achieve. to understand clearly",
+    ]
+]*/
+
+interface IDictionaryIndex {
+    [pinyin: string]: number; // array line number
+}
+
+type PinyinMeaning = { p: string; e: string };
+
+type Dictionary = [PinyinMeaning[]];
